ab5025174e371459f1e17a236960181ae2423778
abdullahelshoura <36606092+abdullahelshoura@users.noreply.github.com>
2018-03-24 12:11:50 +0200

    Update Moving Car.py

--- Moving Car/Moving Car.py
+++ Moving Car/Moving Car.py
@@ -11,8 +11,8 @@ def myinit():
     gluLookAt(10,10,10,0,0,0,0,1,0)
 x=0
 angle=0
-dx=0.003
-dangle=0.3
+dx=0.004
+dangle=0.4
 
 
 
@@ -26,6 +26,21 @@ def draw():
 
     glMatrixMode(GL_MODELVIEW)
 # The Road
+
+
+
+    glColor3f(0, 0, 0)
+    glLoadIdentity()
+    glTranslate(-6, 0, -1 * 34)
+    glScale(0.2, 10, -0.5)
+    glutSolidCube(4.5)
+
+    glColor3f(0, 0, 0)
+    glLoadIdentity()
+    glTranslate(-26, 0, -1 * 22)
+    glScale(0.2, 10, -0.5)
+    glutSolidCube(4.5)
+
     glColor3f(0, 0, 0)
     glLoadIdentity()
     glTranslate(0, 0, -1 * 6)
@@ -65,7 +80,7 @@ def draw():
 
     glMatrixMode(GL_MODELVIEW)
 
-    glColor3f(0, 0, 1)
+    glColor3f(0, 0, 0)
     glLoadIdentity()
     glTranslate(x - 2.5, - 2.5 * .25, (-2.5 * 0.5) - 0.5)
     glRotatef(angle, 0, 0, 1)
@@ -78,7 +93,7 @@ def draw():
     glutSolidCube(4.5)
 
     glLoadIdentity()
-    glColor3f(0,0,1)
+    glColor3f(0,0,0)
     glTranslate(x,0,-0.5)
     glScale(1.0,0.25,0.5)
     glutWireCube(5)
@@ -89,29 +104,38 @@ def draw():
     glScale(0.5, 0.25, 0.5)
     glutSolidCube(5)
 
-    glColor3f(1, 1, 1)
+    glColor3f(0, 0, 0)
     glLoadIdentity()
     glTranslate(x, 0.25 * 5, -0.5)
     glScale(0.5, 0.25, 0.5)
     glutWireCube(4.5)
 
+    glLoadIdentity()
+    glColor3f(0, 0, 0)
+    glTranslate(x+0.5, 0.25 * 3.3, 0.7)
+    glScale(0.15, 0.35, 0)
+    glutSolidCube(5)
 
+    glLoadIdentity()
+    glColor3f(0, 0, 0)
+    glTranslate(x - 0.5, 0.25 * 3.3, 0.7)
+    glScale(0.15, 0.35, 0)
+    glutSolidCube(5)
 
-
-    glColor3f(0,0,1)
+    glColor3f(0,0,0)
 
     glLoadIdentity()
     glTranslate(x+2.5,-2.5*.25,(2.5*.5)-0.5)
     glRotatef(angle,0,0,1)
     glutWireTorus(0.125,0.5,12,8)
 
-    glColor3f(0,0,1)
+    glColor3f(0,0,0)
     glLoadIdentity()
     glTranslate(x+2.5, -2.5 * .25, (-2.5*.5)-0.5)
     glRotatef(angle, 0, 0, 1)
     glutWireTorus(0.125, 0.5, 12, 8)
 
-    glColor3f(0, 0, 1)
+    glColor3f(0, 0, 0)
     glLoadIdentity()
     glTranslate(x-2.5, -2.5 * .25, (2.5*.5)-0.5)
     glRotatef(angle, 0, 0, 1)
@@ -130,14 +154,24 @@ def draw():
     glScale(100, 0.25, 0.000000005)
     glutSolidCube(4)
 
+    glColor3f(0, 0, 0)
+    glLoadIdentity()
+    glTranslate(0, 0, -1 * 6)
+    glScale(0.2, 10, -0.5)
+    glutSolidCube(4.5)
 
+    glColor3f(0, 0, 0)
+    glLoadIdentity()
+    glTranslate(-29, 0, -1 * 6)
+    glScale(0.2, 10, -0.5)
+    glutSolidCube(4.5)
 
     if x > 7:
-        dx = -0.003
-        dangle = -0.3
+        dx = -0.004
+        dangle = -0.4
     elif x < -17:
-        dx= 0.003
-        dangle = 0.3
+        dx= 0.004
+        dangle = 0.4
 
     x += dx
     angle -= dangle
